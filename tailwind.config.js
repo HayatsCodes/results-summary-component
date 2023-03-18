dbed87c25b6ff3b732b005ec7c0da76eb61a95a7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-18 11:31:45 +0100

    Refactoring the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["*.html"],
   theme: {
     extend: {
+      width: {
+        '200': '200px',
+        '300': '300px',
+        '400': '400px',
+      },
       fontSize: {
         '11': '11px',
       },
