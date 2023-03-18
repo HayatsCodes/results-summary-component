40128a5a4a898958e1a5e55ffcff38bc20bbadb2
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-18 11:00:24 +0100

    Updating the documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
+      fontSize: {
+        '11': '11px',
+      },
       colors: {
         'my-color': 'hsl(228, 45%, 44%)',
       },
